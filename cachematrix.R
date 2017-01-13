672e424d1474a7642230c143b1df13f9d5cae521
ryanlandrith <ryanlandrith@gmail.com>
2017-01-13 06:58:44 -0600

    finished assignment for peer review

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix defines a get and set function for both caching the original matrix 
+#and the inverse. It returns a list of the four functions and contains pointers to each 
+#as well as the variables "inv" and "x"
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function()
+        x
+    setinv <- function(inverse)
+        inv <<- inverse
+    getinv <- function()
+        inv
+    list(
+        set = set,
+        get = get,
+        setinv = setinv,
+        getinv = getinv
+    )
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(z, ...) {
+    ## z is an object of type "makeCacheMatrix" i.e. it is a list of functions and contains 
+    #pointers all variables in that environment
+    inv <- z$getinv()
+    if (!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- z$get()
+    inv <-
+        solve(data, ...)# This ... is passed from the function to specifiy solve options
+    z$setinv(inv)
+    inv
 }
